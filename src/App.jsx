9d1cb57c481e089722bfee45eb1dbbb81cc92e07
melasim <melania.simes@hotmail.com>
2023-03-09 00:43:25 -0300

    entrega examen

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,49 @@ import './App.css'
 import Card from './Components/Card'
 
 function App() {
+  const [user, setUser] = useState({
+    duenio: "",
+    mascota:""
+  })
+
+  const [validacion, setValidacion] = useState(false)
+  const [error, setError] = useState(false)
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    if(user.duenio.length > 2 && user.mascota.length >5){
+        setValidacion(true)
+        setError(false)
+    } else {
+      setValidacion(false)
+      setError(true)
+    }
   
+  }
+
+  const handleNombreDuenio = (event) => {
+    const nombreDuenio = event.target.value;
+    const nombreSeteado = nombreDuenio.trim();
+    setUser({...user, duenio : nombreSeteado}); 
+  }
+
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card/>
+      <h1>Ingresá tu Mascota!</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Tu nombre o apodo:</label>
+          <input type="text" value={user.duenio} onChange={handleNombreDuenio}/>
+        </div>
+        <div>
+          <label>Nombre de tu mascota:</label>
+          <input type="text" value={user.mascota} onChange={(e) => setUser({...user, mascota: e.target.value})} />
+        </div>
+        <button type='submit'>Enviar</button>
+      </form>
+
+      {error && "Por favor chequea que la información sea correcta"}
+      {validacion && <Card duenio={user.duenio} mascota={user.mascota}/>}
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
-const Card = () => {
+import '../Card.css'
+const Card = ({duenio, mascota}) => {
+    const date = new Date()
+    const dia = date.getDate()
+    const mes = date.getMonth()
+    const fecharegistro = dia + '/' + mes
+    
     return (
-        <div>Hola! Soy una card</div>
+        <div className='Card'>
+            <h3>Bienvenido {duenio} !</h3>
+            <h3>Hemos registrado a {mascota} en nuestro sistema.</h3>
+            <h4>Fecha de registro: {fecharegistro}</h4>
+        </div>
     )
 }
 
